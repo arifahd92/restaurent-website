df5a3e8d04c8a6b831f34aad792fd1b35b6b260c
md arif <arifahd92@gmail.com>
2023-07-18 16:40:32 +0530

    random

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import Header from "./component/Layout/Header";
 import Meals from "./component/Meals/Meals";
 import Cart from "./component/Cart/Cart";
 import CartProvider from "./store/CartProvider";
-
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
